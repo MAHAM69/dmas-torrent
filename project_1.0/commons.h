bdd5d06950c9d8ed50f98ff891c662a5994d8726
grzegorzszymczak <grzegorzszymczak@139751c5-bc3a-0410-b43d-057cfd0b486a>
2007-09-24 20:22:05 +0000

    przerobione if na switch w ConnectionManager.cc Dodane puste funkcje obslugujace komunikaty (bez implementacji lub czesciowa implementacja)

--- project_1.0/commons.h
+++ project_1.0/commons.h
@@ -1,24 +1,36 @@
 #ifndef COMMONS_H_
 #define COMMONS_H_
 
+
 // constants used as addresses in internal node communication
 static const unsigned int NODE_MANAGER = 0;
 static const unsigned int DATA_MANAGER = 1;
 static const unsigned int CONNECTION_MANAGER = 2;
 
+
+//----------------------------------------------------
 // constants used as types of messages between peers and between submodules
 // handshake message between peers
-static const unsigned int HANDSHAKE_MSG = 0;
+static const unsigned int MSG_HANDSHAKE = 0;
 // message to data manager that connecitns has bee nestablished and data processing may be started
-static const unsigned int CONNECTIONS_ESTABLISHED_MSG = 1;
+static const unsigned int MSG_CONNECTIONS_ESTABLISHED = 1;
 // response for a handshake; message identical to HANDSHAKE_MSG but no response is expected
-static const unsigned int HANDSHAKE_RESPONSE = 2;
+static const unsigned int MSG_HANDSHAKE_RESPONSE = 2;
 
-static const unsigned int BITFIELD_MSG = 3;
+static const unsigned int MSG_BITFIELD = 3;
 // bitfield message sent by connection manager to data manager to fill payload 
-static const unsigned int SELF_BITFIELD_MSG = 4;
+static const unsigned int MSG_SELF_BITFIELD = 4;
 // response for a bitfield; message identical to BITFIELD_MSG but no response is expected
-static const unsigned int BITFIELD_RESPONSE = 5;
+static const unsigned int MSG_BITFIELD_RESPONSE = 5;
+
+//intrested message
+static const unsigned int MSG_INTERESTED = 6;
+
+//choked/unchoked messages
+static const unsigned int MSG_CHOKED = 7;
+static const unsigned int MSG_UNCHOKED = 8;
+
+//---------------------------------------
 
 static const unsigned int NODE_1 = 1;
 static const unsigned int NODE_2 = 2;
@@ -34,5 +46,9 @@ const int NO_PEERS=2;
 //interval in seconds that the client should 
 //wait between sending regular requests to tracker
 const double REQ_INTERVAL=100;
+
+
+//max number of downloads for peer
+const int NO_DOWNLOADS = 1;
 
 #endif /*COMMONS_H*/
