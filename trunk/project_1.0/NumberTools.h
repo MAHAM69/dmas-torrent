dc9cdbe7ca1d04a040a441780206b7eed5a091cb
aleksander.malaszkiewicz <aleksander.malaszkiewicz@139751c5-bc3a-0410-b43d-057cfd0b486a>
2007-09-24 17:14:43 +0000

    Metody do obslugi liczb.

---
+++ trunk/project_1.0/NumberTools.h
@@ -0,0 +1,27 @@
+#ifndef NUMBERTOOLS_H_
+#define NUMBERTOOLS_H_
+
+#include <string>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * @author: Alek Malaszkiewicz
+ */
+class cNumberTools
+{
+private:
+protected:
+public:
+    cNumberTools();
+    virtual ~cNumberTools();
+    int str2int( string );
+    float str2float( string );
+    string int2str( int, int );
+    int getRandomInt( int );
+    float getRandomFloat( float, int );
+};
+
+#endif /*NUMBERTOOLS_H_*/
