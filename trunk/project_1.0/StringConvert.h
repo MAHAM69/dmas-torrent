a8398f0fd8f4f8ef4179f1d6f544cfc661e361ed
aleksander.malaszkiewicz <aleksander.malaszkiewicz@139751c5-bc3a-0410-b43d-057cfd0b486a>
2007-09-25 15:40:20 +0000

    Poprawka do listy odebranej z Trackera. Juz nie powiela nazw. Dodatkowa klasa po ktorej mozna dziedziczyc, aby nie powielac zmiennych typu istringsteam i ostringsteam.

---
+++ trunk/project_1.0/StringConvert.h
@@ -0,0 +1,17 @@
+#ifndef STRINGCONVERT_H_
+#define STRINGCONVERT_H_
+
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class StringConvert
+{
+    private:
+    public:
+	istringstream iss;
+	ostringstream oss;
+};
+
+#endif /*STRINGCONVERT_H*/
